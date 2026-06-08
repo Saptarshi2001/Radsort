a07554ba2380b35f1509c1326f562b28b58e71b5
Saptarshi Dutta <saptarshidutta2001@gmail.com>
2026-06-09 01:37:04 +0530

    reduced memory overhead

--- src/rad.ts
+++ src/rad.ts
@@ -1,28 +1,26 @@
 
-
 export function rxsort(arr:number[]) : number[]{
     if(arr.length==0)
     {
         return [];
     }
-    let expPrefix: Map<number,number[]>=new Map();
-    let queue: number[]=[];
+    let result:number[]=arr;
     let output:number[]=[];
     let maxNumArr:number=Math.max(...arr);
     let exp: number=1;
     let prefixSum:number[]=Array(10).fill(0);
     let freqArr:number[]=Array(10).fill(0);
-    let finalOutput:number[]=[];
     while(maxNumArr>0)
     {
-        queue.push(exp);
-        freqArr=countFreq(arr,exp);
-        prefixSum=calPrefixSum(freqArr,exp,expPrefix);
+        
+        freqArr=countFreq(result,exp);
+        prefixSum=calPrefixSum(freqArr);
+        result=finalArr(result,prefixSum,exp);
         exp*=10;
         maxNumArr=Math.floor(maxNumArr / 10);
     }
-    finalOutput=finalArr(arr,queue,expPrefix);
-    output=finalOutput;
+    
+    output=result;
     return output;
 }
 
@@ -46,7 +44,7 @@ return arrFreq;
 
 }
 
-function calPrefixSum(freqArr:number[],exp:number,expPrefix:Map<number,number[]>): number[]{
+function calPrefixSum(freqArr:number[]): number[]{
 
 if (freqArr.length==0)
 {
@@ -61,33 +59,19 @@ for(let j=1;j<n;j++)
     prefixSum[j]=prefixSum[j-1]!+ freqArr[j]!;
     
 }
-
-expPrefix.set(exp,prefixSum);
 return prefixSum;
 }
 
-function finalArr(arr:number[],queue:number[],expPrefix:Map<number,number[]>):number[]{
+function finalArr(arr:number[],prefixSum:number[],exp:number):number[]{
         
         if(arr.length==0)
 {
     return [];
 }
-        if(queue.length==0)
-{
-    return [];
-}
         let outputArr:number[]=Array(arr.length).fill(0);  
-        let exp:number=queue.shift()!;
-        let prefSum:number[]=expPrefix.get(exp)!;
-        let finalArr:number[]=calculateArr(prefSum,arr,exp); 
-    while(queue.length!=0)
-    {
-        exp=queue.shift()!;
-        prefSum=expPrefix.get(exp)!;
-        finalArr=calculateArr(prefSum,finalArr,exp);        
-    }
-    outputArr=finalArr;
-    return outputArr;
+        let finalArr:number[]=calculateArr(prefixSum,arr,exp); 
+        outputArr=finalArr;
+        return outputArr;
 }
 function calculateArr(prefixSum:number[],arr:number[],exp:number){
     if(arr.length==0)
